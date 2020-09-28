5db2a35668bd319d645d22bebfb67144a3cb5e10
brad vanderbush <brad@brads-Mac-mini.lan>
2020-09-27 18:15:50 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# vue-icon-animate
+
+> Replica of previous project `icon-animate` but for Vue.js projects instead of React.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <react></react>
+  </div>
+</template>
+
+
+<script>
+import React from './React'
+
+export default {
+  name: 'app',
+  components: {
+    'react': React
+  },
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
